8788a66649d750613117f48b744938c911996b2c
Vuyo Malinga <vuyo@butterflyeffect.xyz>
2026-05-17 10:16:48 +0100

    enable image optimization and compression

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        pathname: '/images/**',
-      },
+      { protocol: 'https', hostname: 'cdn.sanity.io', pathname: '/images/**' },
     ],
+    formats: ['image/avif', 'image/webp'],
   },
+  compress: true,
 }
 
 export default nextConfig
